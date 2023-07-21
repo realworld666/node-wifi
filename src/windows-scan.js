35ca4f2f5a153ad7151f4e63ce95cc05cbcb7418
Dave Conley <dave.c@fundamentalvr.com>
2023-07-21 13:00:46 +0100

    Made the scan parsing more robust

--- src/windows-scan.js
+++ src/windows-scan.js
@@ -3,98 +3,137 @@ const networkUtils = require('./utils/network-utils');
 const env = require('./env');
 
 function scanWifi(config, callback) {
-  try {
-    execFile(
-      'netsh',
-      ['wlan', 'show', 'networks', 'mode=Bssid'],
-      { env },
-      (err, scanResults) => {
-        if (err) {
-          callback && callback(err);
-          return;
-        }
+    try {
+        execFile(
+            'netsh',
+            ['wlan', 'show', 'networks', 'mode=Bssid'],
+            {env},
+            (err, scanResults) => {
+                if (err) {
+                    callback && callback(err);
+                    return;
+                }
 
-        scanResults = scanResults
-          .toString('utf8')
-          .split('\r')
-          .join('')
-          .split('\n')
-          .slice(4, scanResults.length);
+                scanResults = scanResults
+                    .toString('utf8')
+                    .split('\r')
+                    .join('')
+                    .split('\n')
+                    .slice(4, scanResults.length);
 
-        let numNetworks = -1;
-        let currentLine = 0;
-        let networkTmp;
-        const networksTmp = [];
-        let network;
-        const networks = [];
-        let i;
+                let numNetworks = -1;
+                let currentLine = 0;
+                let networkTmp;
+                const networksTmp = [];
+                let network;
+                const networks = [];
+                let i;
 
-        for (i = 0; i < scanResults.length; i++) {
-          if (scanResults[i] === '') {
-            numNetworks++;
-            networkTmp = scanResults.slice(currentLine, i);
-            networksTmp.push(networkTmp);
-            currentLine = i + 1;
-          }
-        }
+                for (i = 0; i < scanResults.length; i++) {
+                    if (scanResults[i] === '') {
+                        numNetworks++;
+                        networkTmp = scanResults.slice(currentLine, i);
+                        networksTmp.push(networkTmp);
+                        currentLine = i + 1;
+                    }
+                }
 
-        for (i = 0; i < numNetworks; i++) {
-          // skip empty networks
-          if (networksTmp[i] && networksTmp[i].length > 0) {
-            network = parse(networksTmp[i]);
-            networks.push(network);
-          }
-        }
+                for (i = 0; i < numNetworks; i++) {
+                    // skip empty networks
+                    if (networksTmp[i] && networksTmp[i].length > 0) {
+                        network = parse(networksTmp[i]);
+                        networks.push(network);
+                    }
+                }
 
-        callback && callback(null, networks);
-      }
-    );
-  } catch (e) {
-    callback && callback(e);
-  }
+                callback && callback(null, networks);
+            }
+        );
+    } catch (e) {
+        callback && callback(e);
+    }
 }
 
+// Helper function to extract values using regular expressions
+function extractValue(str, regex) {
+    const trimmedStr = str.trim();
+    const match = trimmedStr.match(regex);
+    return match ? match[1].trim() : null;
+}
+
+
 function parse(networkTmp) {
-  const network = {};
+    const network = {
+        channel: -1,
+        quality: 0,
+        signal_level: Number.MIN_VALUE,
+        security: '',
+        security_flags: ''
+    };
 
-  network.mac = networkTmp[4] ? networkTmp[4].match(/.*?:\s(.*)/)[1] : '';
-  network.bssid = network.mac;
-  network.ssid = networkTmp[0] ? networkTmp[0].match(/.*?:\s(.*)/)[1] : '';
-  network.channel = networkTmp[7]
-    ? parseInt(networkTmp[7].match(/.*?:\s(.*)/)[1])
-    : -1;
-  network.frequency = network.channel
-    ? parseInt(networkUtils.frequencyFromChannel(network.channel))
-    : 0;
-  network.signal_level = networkTmp[5]
-    ? networkUtils.dBFromQuality(networkTmp[5].match(/.*?:\s(.*)/)[1])
-    : Number.MIN_VALUE;
-  network.quality = networkTmp[5]
-    ? parseFloat(networkTmp[5].match(/.*?:\s(.*)/)[1])
-    : 0;
-  network.security = networkTmp[2] ? networkTmp[2].match(/.*?:\s(.*)/)[1] : '';
-  network.security_flags = networkTmp[3]
-    ? networkTmp[3].match(/.*?:\s(.*)/)[1]
-    : '';
-  network.mode = 'Unknown';
+    for (const item of networkTmp) {
 
-  return network;
+        const ssid = extractValue(item, /^SSID \d+ : (.+)/);
+        if (ssid) {
+            network.ssid = ssid;
+            continue;
+        }
+
+        const mac = extractValue(item, /^BSSID .+ : (.+)/);
+        if (mac) {
+            network.mac = mac;
+            continue;
+        }
+
+        const channel = extractValue(item, /^Channel .+ : (\d+)/);
+        if (channel) {
+            network.channel = parseInt(channel);
+            continue;
+        }
+
+        const signalLevel = extractValue(item, /^Signal .+ : (\d+)%/);
+        if (signalLevel) {
+            network.quality = parseInt(signalLevel);
+            network.signal_level = networkUtils.dBFromQuality(signalLevel);
+            continue;
+        }
+
+        const authentication = extractValue(item, /^Authentication\s*:\s*(.+)/);
+        if (authentication) {
+            network.security = authentication;
+            continue;
+        }
+
+        const securityFlags = extractValue(item, /^Encryption\s+:\s+(.+)/);
+        if (securityFlags) {
+            network.security_flags = securityFlags;
+            continue;
+        }
+    }
+
+    network.bssid = network.mac;
+    network.frequency = network.channel !== -1
+        ? parseInt(networkUtils.frequencyFromChannel(network.channel))
+        : 0;
+    network.mode = 'Unknown';
+
+    return network;
 }
 
 module.exports = config => {
-  return callback => {
-    if (callback) {
-      scanWifi(config, callback);
-    } else {
-      return new Promise((resolve, reject) => {
-        scanWifi(config, (err, networks) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(networks);
-          }
-        });
-      });
-    }
-  };
+    return callback => {
+        if (callback) {
+            scanWifi(config, callback);
+        } else {
+            return new Promise((resolve, reject) => {
+                scanWifi(config, (err, networks) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(networks);
+                    }
+                });
+            });
+        }
+    };
 };
